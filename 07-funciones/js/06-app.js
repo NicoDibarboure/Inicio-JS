8574bc099ca1dfbcb83189c185d8e82bbd36ff8d
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-05-22 13:16:15 -0300

    Parametros por default.

---
+++ 07-funciones/js/06-app.js
@@ -0,0 +1,8 @@
+function saluda(
+  nombre = "No ingreso el nombre", // Se le asigna un valor por defecto.
+  apellido = "No ingreso el apellido"
+) {
+  console.log(`Hola ${nombre} ${apellido}`);
+}
+
+saluda("Nicolas", "Dibarboure");
