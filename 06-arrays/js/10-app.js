ef2c1216e75085041b8508bf4e5e2e499b8eadf5
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-05-21 22:02:31 -0300

    .map para iterar un array, y las diferencias con forEach.

--- 06-arrays/js/10-app.js
+++ 06-arrays/js/10-app.js
@@ -1,1 +1,21 @@
+const carrito = [
+  { nombre: "Monitor 27 Pulgadas", precio: 500 },
+  { nombre: "Television", precio: 100 },
+  { nombre: "Tablet", precio: 200 },
+  { nombre: "Audifonos", precio: 300 },
+  { nombre: "Teclado", precio: 400 },
+  { nombre: "Celular", precio: 700 },
+];
 
+// carrito.forEach(function (item) {
+//   console.log(`${item.nombre} - Precio: ${item.precio}`);
+// });
+
+// .map method
+// .map crea un nuevo arreglo con los resultados de la llamada a la función indicada aplicados a cada uno de sus elementos.
+
+const nuevoArray = carrito.map(function (item) {
+  return `${item.nombre} - Precio: ${item.precio}`;
+});
+
+console.log(nuevoArray);
