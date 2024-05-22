11a7f5e902d98e315cb8b7d46a986e1c77dc433e
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-05-22 20:01:37 -0300

    Comparador estricto.

--- 08-Estructuras/js/02-app.js
+++ 08-Estructuras/js/02-app.js
@@ -1,1 +1,17 @@
+const puntaje = 1000;
 
+// if (puntaje != 1000) {
+//   // != Diferente == Igual a
+//   console.log("Si, es diferente");
+// } else {
+//   console.log("No es diferente");
+// }
+
+if (puntaje !== "1000") {
+  // === Compara el valor y el tipo de dato. En este caso, el valor es 1000 y el tipo de dato es number.
+  // !== Compara el valor y el tipo de dato. En este caso, el valor es 1000 y el tipo de dato es string.
+
+  console.log("Si es diferente");
+} else {
+  console.log("No es diferente");
+}
