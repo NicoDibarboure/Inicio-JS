e66ae22eb3bc1839dfd1f0eba0277a53803a3f7c
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-05-22 20:07:26 -0300

    Comparando Mayor o Menor que

--- 08-Estructuras/js/03-app.js
+++ 08-Estructuras/js/03-app.js
@@ -1,1 +1,12 @@
+// Operador Mayor que y Menor que
 
+const dinero = 300;
+const totalAPagar = 300;
+
+if (dinero >= totalAPagar) {
+  console.log("Si podemos pagar");
+} else {
+  console.log("Fondos insuficientes");
+}
+
+// Operador
