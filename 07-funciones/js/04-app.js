3cf6db9e7aec7c353e3cf86d9ff818a6776901d6
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-05-22 13:06:04 -0300

    Diferencia entre Funcion y Método.

--- 07-funciones/js/04-app.js
+++ 07-funciones/js/04-app.js
@@ -1,1 +1,11 @@
+const numero1 = 20;
+const numero2 = "20";
 
+console.log(parseInt(numero2)); // Esto es una función que convierte un string a un número entero.
+
+console.log(numero1.toString()); // Esto es un metodo que convierte un número entero a un string.
+
+function sumar() {
+  console.log(2 + 2);
+}
+sumar();
