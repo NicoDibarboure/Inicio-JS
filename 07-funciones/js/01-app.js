f59128190fc5ecbdb13d12ddc2db81af80112a45
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-05-21 22:13:01 -0300

    Function Declaration & Function Expression.

---
+++ 07-funciones/js/01-app.js
@@ -0,0 +1,18 @@
+// Declaracion de Funcion (Function Declaration)
+
+function sumar(a, b) {
+  console.log(a + b);
+}
+
+// Llamado a la funcion con los parametros.
+sumar(2, 3);
+sumar(200, 184);
+sumar(342, 42);
+
+// Expression de Funcion (Function Expression)
+
+const sumar2 = function () {
+  console.log(3 + 3);
+};
+
+sumar2();
