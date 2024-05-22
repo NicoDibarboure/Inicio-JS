b1f9d2f6059f8b6999dc78e49f7f583c209c82f6
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-05-22 20:11:24 -0300

    Mayor o Igual y else if

--- 08-Estructuras/js/04-app.js
+++ 08-Estructuras/js/04-app.js
@@ -1,1 +1,16 @@
+// Operador Mayor que y Menor que
 
+const dinero = 200;
+const totalAPagar = 300;
+const tarjeta = true;
+const cheque = true;
+
+if (dinero >= totalAPagar) {
+  console.log("Si podemos pagar");
+} else if (cheque) {
+  console.log("Si puedo pagar porque tengo chequera");
+} else if (tarjeta) {
+  console.log("Si puedo pagar porque tengo tarjeta");
+} else {
+  console.log("Fondos insuficientes");
+}
