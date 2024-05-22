3d83f505e78964664b3eda66b0d4c7a125fb4ed3
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-05-22 13:02:20 -0300

    Algunas funciones nativas en JS.

---
+++ 07-funciones/js/03-app.js
@@ -0,0 +1,3 @@
+// alert("Error en la función, revisa la consola."); // Da un mensaje de alerta.
+// prompt("Cual, es tu eadad?"); Ingresas un valor en pantalla
+// console.log(parseInt("20")); // Convierte un string a un número entero.
