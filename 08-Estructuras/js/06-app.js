82878eb28950340950f2cd146bfc450cb726ec48
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-05-22 20:26:11 -0300

    Operador && para revisar que se cumplan 2 o mas condiciones.

--- 08-Estructuras/js/06-app.js
+++ 08-Estructuras/js/06-app.js
@@ -1,1 +1,12 @@
+const usuario = false;
+const puedePagar = true;
 
+if (usuario && puedePagar) {
+  console.log("Si puedes comprar");
+} else if (!usuario) {
+  console.log("Inicia sesión o crea una cuenta");
+} else if (!puedePagar) {
+  console.log("Fondos insuficientes");
+} else if (!usuario && !puedePagar) {
+  console.log("Inicia sesión o crea una cuenta y verifica tus fondos");
+}
