3bbbaf49e1ee98413cef81bf56efb913ba90b584
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-05-22 20:20:23 -0300

    Switch para evaluar multiples Condiciones.

--- 08-Estructuras/js/05-app.js
+++ 08-Estructuras/js/05-app.js
@@ -1,1 +1,21 @@
+// Switch Case
 
+const metodoPago = "cheque";
+
+switch (metodoPago) {
+  case "efectivo":
+    console.log(`Pagaste con ${metodoPago}`);
+    break;
+  case "credito":
+    console.log(`Pagaste con ${metodoPago}`);
+    break;
+  case "débito":
+    console.log(`Pagaste con ${metodoPago}`);
+    break;
+  case "cheque":
+    console.log(`Pagaste con ${metodoPago}`);
+    break;
+  default:
+    console.log("Otro método de pago");
+    break;
+}
