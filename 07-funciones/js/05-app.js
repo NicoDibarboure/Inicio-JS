494e4934e3feff3feee202fe329769c4eb309fff
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-05-22 13:11:42 -0300

    Parametros y argumentos en Funciones.

---
+++ 07-funciones/js/05-app.js
@@ -0,0 +1,15 @@
+function sumar(a, b) {
+  // a y b son parámetros.
+  console.log(a + b);
+  // a y b son argumentos.
+}
+sumar(10, 20);
+sumar(3, 4);
+
+function saluda(nombre, apellido) {
+  console.log(`Hola ${nombre} ${apellido}`);
+}
+
+saluda("Juan", "De la torre");
+saluda("Carlos", "Lara");
+saluda("Laura", "Quintero");
