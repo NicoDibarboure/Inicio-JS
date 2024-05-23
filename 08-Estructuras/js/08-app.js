ab79c3b388ec3953d7245119bc45b5a0c4b89999
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-05-22 23:34:20 -0300

    Detener la ejecucion de un IF con una FUNCION.

--- 08-Estructuras/js/08-app.js
+++ 08-Estructuras/js/08-app.js
@@ -1,1 +1,15 @@
+const puntaje = 350;
 
+function revisarPuntaje() {
+  if (puntaje > 400) {
+    console.log("Buen puntaje, felicidades.");
+    return;
+  }
+
+  if (puntaje > 300) {
+    console.log("No pasaste el puntaje mínimo.");
+    return;
+  }
+}
+
+revisarPuntaje();
