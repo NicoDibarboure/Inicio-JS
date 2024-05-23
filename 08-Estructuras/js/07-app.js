1aa325f5b722cf844e47820a669bb0a5139239f8
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-05-22 23:26:11 -0300

    Operador OR || para que se cumpla al menos una condicion.

--- 08-Estructuras/js/07-app.js
+++ 08-Estructuras/js/07-app.js
@@ -1,1 +1,10 @@
+const efectivo = 300;
+const credito = 400;
+const disponible = efectivo + credito;
+const totalPagar = 600;
 
+if (totalPagar < efectivo || totalPagar < credito || totalPagar < disponible) {
+  console.log("Si podemos pagar");
+} else {
+  console.log("Fondos insuficientes");
+}
