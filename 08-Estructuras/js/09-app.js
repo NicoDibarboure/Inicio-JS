42f4d6ac9e66befcb84203c27f8cf4cce37607cb
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-05-22 23:43:00 -0300

    Operador Ternario.

--- 08-Estructuras/js/09-app.js
+++ 08-Estructuras/js/09-app.js
@@ -1,1 +1,29 @@
+const autenticado = true;
+const puedePagar = true;
 
+// console.log(
+//   autenticado && puedePagar ? "Si puedes ver Netflix" : "No puedes ver Netflix"
+// );
+
+console.log(
+  autenticado
+    ? puedePagar
+      ? "Si puedes ver Netflix"
+      : "No puedes ver Netflix"
+    : "Necesitas estar autenticado"
+);
+
+// const efectivo = 800;
+// const credito = 400;
+// const disponible = efectivo + credito;
+// const totalPagar = 600;
+
+// if (efectivo > totalPagar || credito > totalPagar || disponible > totalPagar) {
+//   if (efectivo > totalPagar) {
+//     console.log("Si pagaste con efectivo");
+//   } else {
+//     console.log("No puedes pagar con efectivo");
+//   }
+// } else {
+//   console.log("Fondos insuficientes");
+// }
