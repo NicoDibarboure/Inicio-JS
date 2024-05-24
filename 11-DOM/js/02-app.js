f71b924ea88c6f73a2f86ac84c6ee9a100d85a3e
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-05-24 15:58:47 -0300

    .etElementsByClassName

---
+++ 11-DOM/js/02-app.js
@@ -0,0 +1,15 @@
+// Seleccionar elementos por su clase.
+
+const header = document.getElementsByClassName("header");
+
+console.log(header);
+
+const hero = document.getElementsByClassName("hero");
+
+console.log(hero);
+
+// Si las clases existen más de una vez, se seleccionan todas las clases con ese nombre.
+
+const contenedores = document.getElementsByClassName("hero");
+
+console.log(hero);
