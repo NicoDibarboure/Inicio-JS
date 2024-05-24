b7e078b4db8e71bd346b081724fa6a8cd91d5180
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-05-24 15:50:02 -0300

    Como acceder a los elementos HTML con "document."

---
+++ 11-DOM/js/01-app.js
@@ -0,0 +1,20 @@
+elemento = document;
+elemento = document.all;
+elemento = document.head;
+elemento = document.body;
+elemento = document.domain;
+elemento = document.URL;
+
+elemento = document.forms;
+elemento = document.forms[0];
+
+elemento = document.links;
+elemento = document.links[8];
+elemento = document.links[8].classList;
+elemento = document.links[8].className;
+
+elemento = document.images;
+
+elemento = document.scripts;
+
+console.log(elemento);
