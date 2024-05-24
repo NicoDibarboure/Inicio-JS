4018d8bf7dd5e74bc60073b406b958568bc7a0b0
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-05-23 21:12:26 -0300

    While Loop

---
+++ 09-Iteradores/js/04-app.js
@@ -0,0 +1,11 @@
+let i = 0; // Inicializar el While Loop
+
+while (i < 10) {
+  if (i % 2 === 0) {
+    console.log(`El numero ${i} es par.`);
+  } else {
+    console.log(`El numero ${i} es impar.`);
+  }
+
+  i++; // Incrementar el While Loop
+}
