1dcd296f08fe56494b5094cad70156d7ced97134
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-05-24 14:00:53 -0300

    .reduce

---
+++ 10-ArrayMethods/js/03-app.js
@@ -0,0 +1,13 @@
+const carrito = [
+  { nombre: "Monitor 27 Pulgadas", precio: 500 },
+  { nombre: "Television", precio: 100 },
+  { nombre: "Tablet", precio: 200 },
+  { nombre: "Audifonos", precio: 300 },
+  { nombre: "Teclado", precio: 400 },
+  { nombre: "Celular", precio: 700 },
+];
+
+// Con un reduce
+
+const suma = carrito.reduce((a, b) => a + b.precio, 0);
+console.log(suma);
