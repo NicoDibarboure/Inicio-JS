1b7a3c9fd191de175fc8bbf72dd91f6558b0384d
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-05-28 12:07:06 -0300

    Cuando el HTML está listo.

---
+++ 11-Eventos/js/01-app.js
@@ -0,0 +1,8 @@
+// Eventos: Cuando el HTML está listo.
+console.log(1);
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(2);
+});
+
+console.log(3);
---
+++ 11-Eventos/js/script.js
@@ -0,0 +1,9 @@
+// Eventos en el DOM. Eventos de carga y descarga
+
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector("#navlist");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
+};
